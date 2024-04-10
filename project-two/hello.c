a8ea46a0e75f16960303d117e06801c6e4f88385
Noah Storms <70107291+storms421@users.noreply.github.com>
2024-04-10 13:20:31 -0600

    Rename functions in hello.c
    Conflicts with reserved names, so there was a rename of init_module and cleanup_module functions in the file to initialization_module and cleaning_module.

--- project-two/hello.c
+++ project-two/hello.c
@@ -1,16 +1,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init_module(void) {
+int initalization_module(void) {
 	printk(KERN_INFO "Hello world!\n");
 	return 0;
 }
 
-void cleanup_module(void){
+void cleaning_module(void){
 	printk(KERN_INFO "Goodbye world!\n");
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(initalization_module);
+module_exit(cleaning_module);
 
 MODULE_LICENSE("GPL");
