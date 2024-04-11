f91d835c2bf16ce8b920874d240ae487ef93cffa
Noah Storms <70107291+storms421@users.noreply.github.com>
2024-04-11 11:55:16 -0600

    Update print_self.c
    Added function get_state

--- project-two/module-two/print_self.c
+++ project-two/module-two/print_self.c
@@ -4,16 +4,39 @@
 
 int __init print_self_init(void) {
 	struct task_struct *task = current;
+
+	const char *get_state(int state) {
+    switch (state) {
+        case 0:
+            return "TASK_RUNNING";
+        case 1:
+            return "TASK_INTERRUPTIBLE";
+        case 2:
+            return "TASK_UNINTERRUPTIBLE";
+        case 4:
+            return "TASK_STOPPED";
+        case 8:
+            return "TASK_TRACED";
+        case 16:
+            return "EXIT_ZOMBIE";
+        case 32:
+            return "EXIT_DEAD";
+        case 64:
+            return "TASK_NONINTERACTIVE";
+        default:
+            return "Unknown";
+    }
+}
 	
 	printk(KERN_INFO "PID: %d\n", task->pid);
 	printk(KERN_INFO "Process Name: %s\n ", task->comm);
-	printk(KERN_INFO "Process State: %ld\n", task->state);
+	printk(KERN_INFO "Process State: %ld\n", get_state(task->state));
 	
 	while (task->parent != task) {
 		task = task->parent;
 		printk(KERN_INFO "Parent PID: %d\n", task->pid);
 		printk(KERN_INFO "Parent Process Name: %s\n", task->comm);
-		printk(KERN_INFO "Parent Process State: %ld\n", task->state);
+		printk(KERN_INFO "Parent Process State: %ld\n", get_state(task->state));
 	}
 
 	return 0;
